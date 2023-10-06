833dce383a4992df7124f4bdefcdf77c8d53a62b
ten4roid <thdckdtjr8@gmail.com>
2023-10-06 15:31:46 +0900

    Update

--- connecTo_py/utils/check_files.py
+++ connecTo_py/utils/check_files.py
@@ -11,7 +11,7 @@ def get_file_list():
 def make_info(file_list, total_file_cnt):
     info = f"### Chapter_01\nTotal Practice Count: {total_file_cnt}개\n"
     for file in file_list:
-        temp = f"- [{file}](https://github.com/xoxlo/data_structure_to_python_easy_practice/tree/main/{file})\n"
+        temp = f"- [{file}](https://github.com/xoxlo/data_structure_to_python_easy_practice/tree/main/Chapter_1/{file})\n"
         info += temp
     return info
     
