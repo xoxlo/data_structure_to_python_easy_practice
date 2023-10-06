7fa17fd7c9fe09ba0cc368c4e999a7ff71e10571
ten4roid <thdckdtjr8@gmail.com>
2023-10-06 16:49:12 +0900

    UPDATE

--- utils/check_files.py
+++ utils/check_files.py
@@ -22,17 +22,6 @@ def make_read_me(info):
 각 챕터별 코드 실습이나 연습 문제 위주로 올립니다.
 
 {info}
-## Chapter_02
-## Chapter_03
-## Chapter_04
-## Chapter_05
-## Chapter_06
-## Chapter_07
-## Chapter_08
-## Chapter_09
-## Chapter_10
-## Chapter_11
-## Chapter_12
 """
 
 
