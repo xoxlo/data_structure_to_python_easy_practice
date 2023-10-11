70ab9ccbcf1299bec6dbd66ec628eb656d1d7d72
ten4roid <thdckdtjr8@gmail.com>
2023-10-11 12:46:18 +0900

    updae

--- Chapter_3/training_3_05.py
+++ Chapter_3/training_3_05.py
@@ -9,15 +9,29 @@ while True:
         pos = int(input(" 입력행 번호 : "))
         str = input(" 입력행 내용 : ")
         list.insert(pos,str)
+        
     elif command == 'd':
         pos = int(input(" 삭제행 번호 : "))
         list.delete(pos)
+    
+    elif command == 'r':
+        pos = int(input(" 변경행 번호 : "))
+        str = input(" 변경행 내용 : ")
+        list.replace(pos, str)
+        
+    elif command == 'p':
+        print('Line Editor')
+        for line in range(list.size):
+            print('[%2d] '%line, end='')
+            print(list.getEntry(line))
+        print()
+        
     elif command == 'q': exit()
     
     elif command == 'l':
         filename = 'test.txt'
         infile = open(filename, "r")
-        lines = infile.readlines();
+        lines = infile.readlines()
         for line in lines:
             list.insert(list.size, line.rstrip('\n'))
         infile.close()
