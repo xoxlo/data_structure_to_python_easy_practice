69ab13c20e9ab9f86240d029ac458b5b5c71a49b
ten4roid <thdckdtjr8@gmail.com>
2023-10-22 12:09:05 +0900

    ch5 update

---
+++ Chapter_5/training_5_04.py
@@ -0,0 +1,18 @@
+from training_5_01_03 import CircularQueue
+
+q = CircularQueue(8)
+q.enqueue('A')
+q.enqueue('B')
+q.enqueue('C')
+q.enqueue('D')
+q.enqueue('E')
+q.enqueue('F')
+print('A B C D E F 삽입 ',q)
+print('삭제 ---> ',q.dequeue())
+print('삭제 ---> ',q.dequeue())
+print('삭제 ---> ',q.dequeue())
+print('3번의 삭제 : ',q)
+q.enqueue('G')
+q.enqueue('H')
+q.enqueue('I')
+print('G H I 삽입 : ',q)
