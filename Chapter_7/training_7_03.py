d1c5fca54aded1743e3771a26c626064d73178ba
ten4roid <thdckdtjr8@gmail.com>
2023-11-06 14:45:20 +0900

    ch7 update

--- Chapter_7/training_7_03.py
+++ Chapter_7/training_7_03.py
@@ -1,12 +1,12 @@
 from training_7_01 import printStep
 
-def bubble_sort(A):
+def bubble_sort(A): # 버블 정렬
     n = len(A)
-    for i in range(n-1, 0, -1):
+    for i in range(n-1, 0, -1): # 인덱스 n-1부터 0까지 비교 한다는 뜻
         bChanged = False
-        for j in range(i):
-            if (A[j]>A[j+1]):
-                A[j], A[j+1] = A[j+1], A[j]
+        for j in range(i): # i만큼 반복
+            if (A[j]>A[j+1]): # 앞에 인덱스가 뒤에 인덱스보다 크면
+                A[j], A[j+1] = A[j+1], A[j] # 둘이 서로 교환
                 bChanged = True
         if not bChanged:
             break
