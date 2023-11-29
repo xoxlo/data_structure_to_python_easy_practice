e68e269ae3aa0a62741b36ddb4bc495243efd681
ten4roid <thdckdtjr8@gmail.com>
2023-11-29 13:27:03 +0900

    update

--- Chapter_9/training_9_05.py
+++ Chapter_9/training_9_05.py
@@ -1,9 +1,19 @@
-def search_max_bst(n): # 최대 값의 노드 탐색
+def search_max_bst(n): # 최대 값의 노드 탐색 - 반복
     while n != None and n.right != None:
         n = n.right
     return n
 
-def search_min_bst(n): # 최소 값의 노드 탐색
+def search_min_bst(n): # 최소 값의 노드 탐색 - 반복
     while n != None and n.left != None:
         n = n.left
     return n
+
+# def search_max_bst(n): # 최소 값의 노드 탐색 - 순환
+#    while n != None and n.right != None:
+#        n = search_max_bst(n.right)
+#    return n
+
+# def search_min_bst(n): # 최소 값의 노드 탐색 - 순환
+#    while n != None and n.left != None:
+#        n = search_min_bst(n.left)
+#    return n
