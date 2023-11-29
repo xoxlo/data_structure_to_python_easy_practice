1711c89ec58728aa8739f17e44d6e794087687ee
ten4roid <thdckdtjr8@gmail.com>
2023-11-29 12:43:12 +0900

    update

--- Chapter_9/training_9_16.py
+++ Chapter_9/training_9_16.py
@@ -27,7 +27,7 @@ def count_leaf(n): # 단말 노드 수 계산 함수
     else:
         return count_leaf(n.left) + count_leaf(n.right) # 비단말 노드 -> 좌 + 우 결과 합
     
-node = [7,8,9,2,1,5,3,6,4]
+node = [0,1,2,3,4,5,6,7,8,9]
 root = None
 for i in node:
     n = BSTNode(i)
