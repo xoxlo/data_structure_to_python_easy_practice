d1e7a696c6ad937524f77fd5a6825325b731055e
ten4roid <thdckdtjr8@gmail.com>
2023-11-29 13:28:32 +0900

    update

--- Chapter_9/training_9_05.py
+++ Chapter_9/training_9_05.py
@@ -8,7 +8,7 @@ def search_min_bst(n): # 최소 값의 노드 탐색 - 반복
         n = n.left
     return n
 
-# def search_max_bst(n): # 최소 값의 노드 탐색 - 순환
+# def search_max_bst(n): # 최대 값의 노드 탐색 - 순환
 #    while n != None and n.right != None:
 #        n = search_max_bst(n.right)
 #    return n
