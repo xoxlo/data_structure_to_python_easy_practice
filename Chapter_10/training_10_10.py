c09ed8a44aeea47f4afe55fe1f1ac40158af2114
ten4roid <thdckdtjr8@gmail.com>
2023-12-08 17:32:36 +0900

    update

--- Chapter_10/training_10_10.py
+++ Chapter_10/training_10_10.py
@@ -1,10 +1,10 @@
-def ST_DFS(vtx, adj, s, visited):
-    visited[s] = True
-    for v in range(len(vtx)):
-        if adj[s][v] != 0:
-            if visited[v] == False:
-                print("(",vtx[s], vtx[v], ")", end=' ')
-                ST_DFS(vtx, adj, v, visited)
+def ST_DFS(vtx, adj, s, visited): # 신장트리 DFS 방법
+    visited[s] = True # 현재 s 정점 방문 표시
+    for v in range(len(vtx)): # 정점의 수 만큼 반복
+        if adj[s][v] != 0: # 정점 s, v가 연결이 되어있다면
+            if visited[v] == False: # 그리고 정점 v를 방문하지 않았다면
+                print("(",vtx[s], vtx[v], ")", end=' ') # s v 출력
+                ST_DFS(vtx, adj, v, visited) # 순환 반복 -> 정점 v를 시작점으로
                 
                 
 vtx = ['A','B','C','D','E','F','G','H']
