54e15770fb7395ab2b7c01f413b6f374da5fee69
ten4roid <thdckdtjr8@gmail.com>
2023-12-08 17:35:32 +0900

    update

--- utils/check_files.py
+++ utils/check_files.py
@@ -118,7 +118,18 @@ def make_info9(file_list):
         temp = f"- [{file}](https://github.com/xoxlo/data_structure_to_python_easy_practice/tree/main/Chapter_9/{file})\n"
         info += temp
     return info
-    
+
+def get_file_list10():
+    file_list = [file for file in os.listdir("./Chapter_10/") if '.py' in file]
+    file_list = sorted(file_list)
+    return file_list
+  
+def make_info10(file_list):
+    info = f"## Chapter_10\n\n"
+    for file in file_list:
+        temp = f"- [{file}](https://github.com/xoxlo/data_structure_to_python_easy_practice/tree/main/Chapter_10/{file})\n"
+        info += temp
+    return info
 
 def make_read_me(info):
     return f"""
@@ -135,7 +146,7 @@ def make_read_me(info):
 
 <img src="https://github.com/xoxlo/data_structure_to_python_easy_practice/assets/46445480/c4624456-228e-4d6a-a0d9-5c50ba664ee8" width="500" height="700"/>
 
-각 챕터별 코드 실습이나 연습 문제 위주로 올립니다.
+각 챕터별 코드 실습 및 연습 문제 위주
 
 {info}
 """
@@ -152,7 +163,8 @@ def update_readme():
     file_list7 = get_file_list7()
     file_list8 = get_file_list8()
     file_list9 = get_file_list9()
-    info = make_info_prac(file_list_prac) + make_info(file_list) + make_info2(file_list2) + make_info3(file_list3) + make_info4(file_list4) + make_info5(file_list5) + make_info6(file_list6) + make_info7(file_list7) + make_info8(file_list8) + make_info9(file_list9)
+    file_list10 = get_file_list10()
+    info = make_info_prac(file_list_prac) + make_info(file_list) + make_info2(file_list2) + make_info3(file_list3) + make_info4(file_list4) + make_info5(file_list5) + make_info6(file_list6) + make_info7(file_list7) + make_info8(file_list8) + make_info9(file_list9) + make_info10(file_list10)
     readme = make_read_me(info)
     return readme
 
