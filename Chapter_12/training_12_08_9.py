93330bd3f0438e0f4cd81de891368789829031e0
ten4roid <thdckdtjr8@gmail.com>
2023-12-17 19:09:35 +0900

    update

--- Chapter_12/training_12_08_9.py
+++ Chapter_12/training_12_08_9.py
@@ -1,13 +1,13 @@
-def quick_sort(A, left, right):
-    if left < right:
-        q = partition(A, left, right)
-        quick_sort(A, left, q-1)
-        quick_sort(A, q+1, right)
+def quick_sort(A, left, right): # 퀵 정렬 함수
+    if left < right: # 정렬 요소가 두 개 이상인 경우만 처리
+        q = partition(A, left, right) # 피벗을 중심으로 두 부분으로 분할
+        quick_sort(A, left, q-1) # 왼쪽 부분 리스트 정렬
+        quick_sort(A, q+1, right) # 오른쪽 부분 리스트 정렬
 
-def partition(A, left, right):
-    low = left + 1
-    high = right
-    pivot = A[left]
+def partition(A, left, right): # 피벗을 나누는 함수
+    low = left + 1 # 왼쪽 부분 리스트의 인덱스 ( 오른쪽으로 증가 방향 )
+    high = right # 오른쪽 부분 리스트의 인덱스 ( 왼족으로 감소 방향 )   
+    pivot = A[left] # 피벗 설정
     while (low <= high):
         while low <= right and A[low] <= pivot:
             low += 1
